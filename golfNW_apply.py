e862f4b37c971e0232a31ff214d056ed629305dc
Shota Yamane <shaw622.dev@gmail.com>
2019-07-05 02:43:41 +0900

    Update golfNW_apply.py
    Simplified some codes.

--- golfNW_apply.py
+++ golfNW_apply.py
@@ -17,50 +17,36 @@ browser = webdriver.Chrome()
 browser.get(url)
 
 #個人情報への同意
-agree = browser.find_element_by_name('multiAnswer(ANSWER'+ str(number) +')')
-agree.click()
+browser.find_element_by_name('multiAnswer(ANSWER'+ str(number) +')').click()
 
-#名前の入力
-familyName = browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 1)+'-1)')
-familyName.send_keys('name')
-lastName = browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 1)+'-2)')
-lastName.send_keys('name')
+#Name
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 1)+'-1)').send_keys('name')
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 1)+'-2)').send_keys('name')
 
-#性別
-sex = browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 2)+')')
-sex.click()
+#Sex
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 2)+')').click()
 
-#年代
-age = browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 3)+')')
-age.click()
+#Age
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 3)+')').click()
 
-eMail = browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 4)+')')
-eMail.send_keys('eMail')
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 4)+')').send_keys('eMail')
 
-postalCode1 = browser.find_element_by_name('singleAnswer(ANSWER'+str(number +5)+'-1)')
-postalCode1.send_keys('postalCode')
-postalCode2 = browser.find_element_by_name('singleAnswer(ANSWER'+str(number +5)+'-2)')
-postalCode2.send_keys('postalCode')
+#ZIP
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number +5)+'-1)').send_keys('postalCode')
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number +5)+'-2)').send_keys('postalCode')
 
-#住所
-address1 = browser.find_element_by_name('singleAnswer(ANSWER'+str(number +6)+')')
-address1.send_keys('address')
-address2 = browser.find_element_by_name('singleAnswer(ANSWER'+str(number +7)+')')
-address2.send_keys('address')
-address3 = browser.find_element_by_name('singleAnswer(ANSWER'+str(number +8)+')')
-address3.send_keys('address')
+#Address
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number +6)+')').send_keys('yourAddress1')
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number +7)+')').send_keys('yourAddress2')
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number +8)+')').send_keys('yourAddress3')
 
-#電話番号
-phone1 = browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 10)+'-1)')
-phone1.send_keys('phone')
-phone2 = browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 10)+'-2)')
-phone2.send_keys('phone')
-phone3 = browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 10)+'-3)')
-phone3.send_keys('phone')
+#Phone
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 10)+'-1)').send_keys('phone#1')
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 10)+'-2)').send_keys('phone#2')
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 10)+'-3)').send_keys('phone#3')
 
-#視聴方法
-age = browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 11)+')')
-age.click()
+#Way
+browser.find_element_by_name('singleAnswer(ANSWER'+str(number + 11)+')').click()
 
 
 
